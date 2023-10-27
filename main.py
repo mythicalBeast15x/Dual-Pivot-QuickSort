730b8731a05c7234c90194d07c421c95e72aae11
lovedivineonwulata <ldonwulata@gmail.com>
2023-10-27 00:10:25 -0400

    Committing the partition function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("HelloWorld")
+def partition(array, low, high):
+    pivot = array[high]
+    i = low
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i+1], array[high] = array[high], array[i+1]
+    return i + 1
+
+def intro_sort(array):
+    max_depth = 2*len(array).bit_length()
